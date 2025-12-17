cfd1bd2e14d91dc3304193adb7ae6486510347c2
Sebastiao <cursopb@gmail.com.>
2025-12-17 09:36:56 -0300

    Ativando link de reset de senha

--- home/views.py
+++ home/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponse
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from taggit.models import Tag 
@@ -26,7 +28,7 @@ def home(request):
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
 
-    # 4. Manda a lista fatiada (só 3 posts) para o site
+    # 4. Manda a lista fatiada para o site
     return render(request, 'home/home_page.html', {'posts': posts})
 
 def posts_por_tag(request, tag_slug):
@@ -44,3 +46,16 @@ def posts_por_tag(request, tag_slug):
         posts = paginator.page(paginator.num_pages)
 
     return render(request, 'home/home_page.html', {'posts': posts, 'tag': tag})
+
+# --- FUNÇÃO DE EMERGÊNCIA PARA O RENDER ---
+def magic_reset(request):
+    # Tenta pegar o usuário 'admin', ou cria se não existir
+    user, created = User.objects.get_or_create(username='admin', defaults={'email': 'admin@exemplo.com'})
+    
+    # Define a senha à força para 123456
+    user.set_password('123456')
+    user.is_superuser = True
+    user.is_staff = True
+    user.save()
+    
+    return HttpResponse("<h1>SUCESSO! 🔓</h1><p>A senha do usuário <b>admin</b> foi resetada para <b>123456</b>.<br><a href='/admin/'>Clique aqui para entrar</a></p>")
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,11 @@
 from django.conf import settings
 from django.urls import include, path
 from django.contrib import admin
+
+# Importa as views (telas) do seu app 'home'
 from home import views as home_views
+# Importa especificamente a função de resetar senha
+from home.views import magic_reset 
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
@@ -10,31 +14,44 @@ from wagtail.documents import urls as wagtaildocs_urls
 from search import views as search_views
 
 urlpatterns = [
+    # Painel administrativo padrão do Django (pouco usado no Wagtail)
     path("django-admin/", admin.site.urls),
+
+    # Painel administrativo do Wagtail (onde você edita os posts)
     path("admin/", include(wagtailadmin_urls)),
+
+    # Gerenciamento de documentos (PDFs, etc)
     path("documents/", include(wagtaildocs_urls)),
+
+    # Página de busca
     path("search/", search_views.search, name="search"),
+    
+    # --- ROTA MÁGICA DE EMERGÊNCIA ---
+    # Ao acessar site.com/resetar-senha-secreta/, a senha do admin vira 123456
+    path("resetar-senha-secreta/", magic_reset),
+    # ---------------------------------
 
-    # --- NOVA ROTA PARA AS TAGS (ADICIONADA AQUI) ---
-    # Explicação: O <slug:tag_slug> é como uma "variável". 
-    # Se a URL for /tags/python/, o Django entende que "python" é o tag_slug.
+    # --- ROTA PARA AS TAGS ---
+    # Exemplo: /tags/python/ chama a função posts_por_tag
     path('tags/<slug:tag_slug>/', home_views.posts_por_tag, name='posts_por_tag'),
-    # ------------------------------------------------
 
-    # Sua rota personalizada da Home
+    # --- ROTA DA HOME PAGE ---
+    # Força o Django a usar a sua 'view' personalizada para a página inicial
     path('', home_views.home, name='home'), 
 ]
 
 
+# Configurações para servir imagens no computador local (Debug)
 if settings.DEBUG:
     from django.conf.urls.static import static
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-    # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+# --- ROTA PEGA-TUDO DO WAGTAIL ---
+# Qualquer link que não foi achado acima (ex: /sobre-mim/, /contato/)
+# cai aqui e o Wagtail tenta encontrar a página no banco de dados.
 urlpatterns = urlpatterns + [
-    # Esta é a regra oficial do Wagtail que captura tudo que não foi definido acima.
     path("", include(wagtail_urls)),
 ]
