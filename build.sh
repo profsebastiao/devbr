1079a444addb6e81c2fc7c7c32ab85b4a19ae33b
Sebastiao <cursopb@gmail.com.>
2025-12-17 09:11:18 -0300

    Script com Auto-Admin

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 # Sair se der erro
 set -o errexit
 
-# Instalar dependências
+# Instalar tudo
 pip install -r requirements.txt
-
-# Converter arquivos estáticos (CSS/Imagens)
 python manage.py collectstatic --no-input
-
-# Atualizar o banco de dados
 python manage.py migrate
+
+# --- O PULO DO GATO (CRIAR ADMIN) ---
+# Este comando verifica se o 'admin' existe. Se não existir, cria com a senha '123456'
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@exemplo.com', '123456')"
